baa26bd784f42f9a19306395f8a403c41dbf9b80
gmferland <gmferland@gmail.com>
2017-08-16 00:21:58 -0400

    added feature title bar back in

--- content-jumbotron.php
+++ content-jumbotron.php
@@ -6,4 +6,10 @@
       <img src="<?php echo get_bloginfo( 'template_directory' );?>/images/ty_default_thumbnail.jpg" />
     <?php } ?>
   </div>
+  <a href="<?php the_permalink(); ?>">
+    <div class="col-xs-12 ty_featured_caption">
+      <h1><?php the_title(); ?></h1>
+      <h2>By <?php the_author(); ?></h2>
+    </div>
+  </a>
 </div>
